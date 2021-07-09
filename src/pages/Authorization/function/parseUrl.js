bdd7dd7c60630e2361d842669eae1560199ec310
Vlad <v.a.konkov@mail.ru>
2021-07-09 21:12:47 +0500

    ор

--- src/pages/Authorization/function/parseUrl.js
+++
@@ -1,9 +0,0 @@
-export default function (url ) {
-    let indexFirstAT = url.indexOf("access_token=") + 13;
-    let indexLastAT = url.indexOf("&expires_in");
-    let indexFirstUI = url.indexOf("user_id=") + 8;
-    let indexLastUI = url.length;
-    let accessToken = url.slice(indexFirstAT, indexLastAT);
-    let userId = url.slice(indexFirstUI, indexLastUI);
-    return {accessToken: accessToken, userId: userId};
-}
--- src/utils/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import {Route} from "react-router-dom";
-
-const PrivateRoute = ({component}) => {
-    return(
-        <Route>
-            {component}
-        </Route>
-    )
-}
